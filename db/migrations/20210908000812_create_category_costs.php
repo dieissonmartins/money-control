b60fd97f105c543a7203770f7a5af3d35a204495
Dieisson Martins <dieisson.martins.santos@gmail.com>
2021-09-09 20:39:24 -0300

    created migrate category costs

--- db/migrations/20210908000812_create_category_costs.php
+++ db/migrations/20210908000812_create_category_costs.php
@@ -5,19 +5,17 @@ use Phinx\Migration\AbstractMigration;
 
 final class CreateCategoryCosts extends AbstractMigration
 {
-    /**
-     * Change Method.
-     *
-     * Write your reversible migrations using this method.
-     *
-     * More information on writing migrations is available here:
-     * https://book.cakephp.org/phinx/0/en/migrations.html#the-change-method
-     *
-     * Remember to call "create()" or "update()" and NOT "save()" when working
-     * with the Table class.
-     */
-    public function change(): void
-    {
+   public function up()
+   {
+        $this->table('category_costs')
+            ->addColumn('name','string')
+            ->addColumn('created_at','datetime')
+            ->addColumn('updated_at','datetime')
+            ->save();
+   }
 
-    }
+   public function down()
+   {
+        $this->dropTable('category_costs');
+   }
 }
