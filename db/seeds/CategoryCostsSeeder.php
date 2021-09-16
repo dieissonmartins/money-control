9528281c194261f2b56ee58f9790ed88b8397b3c
Dieisson Martins <dieisson.martins.santos@gmail.com>
2021-09-16 20:48:28 -0300

    created seeder CategoryConsts

---
+++ db/seeds/CategoryCostsSeeder.php
@@ -0,0 +1,56 @@
+<?php
+
+use Phinx\Seed\AbstractSeed;
+
+final class CategoryCostsSeeder extends AbstractSeed
+{
+    public function run()
+    {
+        $categoryCosts = $this->table('category_costs');
+
+        $categoryCosts->insert([
+            [
+                'name' => 'Categoria 1',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s')
+            ],
+            [
+                'name' => 'Categoria 2',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s')
+            ],
+            [
+                'name' => 'Categoria 3',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s')
+            ],
+            [
+                'name' => 'Categoria 4',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s')
+            ],
+            [
+                'name' => 'Categoria 5',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s')
+            ],
+            [
+                'name' => 'Categoria 6',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s')
+            ],
+            [
+                'name' => 'Categoria 7',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s')
+            ],
+            [
+                'name' => 'Categoria 8',
+                'created_at' => date('Y-m-d H:i:s'),
+                'updated_at' => date('Y-m-d H:i:s')
+            ]
+        ]);
+
+        $categoryCosts->save();
+    }
+}
