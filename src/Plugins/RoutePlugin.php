9cc5f5b9f1e4559e69cafc95a2cf40c1aa399943
Dieisson Martins <dieisson.martins.santos@gmail.com>
2021-09-23 20:18:06 -0300

    finish route plugin

--- src/Plugins/RoutePlugin.php
+++ src/Plugins/RoutePlugin.php
@@ -1,9 +1,14 @@
 <?php
+declare(strict_types=1);
 
 namespace Src\Plugins;
 
 use Aura\Router\RouterContainer;
+use Cake\Core\ContainerInterface;
+use Pimple\Container;
+use Psr\Http\Message\RequestInterface;
 use Src\ServiceContainerInterface;
+use Zend\Diactoros\ServerRequestFactory;
 
 class RoutePlugin implements PluginInterface
 {
@@ -20,7 +25,25 @@ class RoutePlugin implements PluginInterface
         /* generate link by base router registered  */
         $generator = $routerContainer->getGenerator();
 
+        $request = $this->getRequest();
+
         $container->add('routing', $map);
         $container->add('routing.matcher', $matcher);
+        $container->add('routing.generator', $generator);
+        $container->add(RequestInterface::class, $request);
+
+        $container->addLazy('route', function (ContainerInterface $container) {
+            $matcher = $container->get('routing.matcher');
+            $request = $container->get(RequestInterface::class);
+
+            return $matcher->match($request);
+        });
+    }
+
+    protected function getRequest(): RequestInterface
+    {
+        return ServerRequestFactory::fromGlobals(
+            $_SERVER, $_GET, $_POST, $_COOKIE, $_FILES
+        );
     }
 }
