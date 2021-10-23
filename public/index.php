06786e79afe0f1e6b428bffffeffcae822e82d44
Dieisson Martins <dieisson.martins.santos@gmail.com>
2021-10-23 20:04:03 -0300

    created layout home default

--- public/index.php
+++ public/index.php
@@ -23,23 +23,8 @@ $app->get('/', function(RequestInterface $request) use ($app) {
     
     $view = $app->service('view.render');
    
-    return $view->render('teste.html.twig', [
-        'name' => 'Dieison Martins'
-    ]); 
+    return $view->render('layout.html.twig'); 
     
 });
-
-$app->get('/home/{name}', function(ServerRequestInterface $request) {
-    
-    $response = new Response();
-    
-    $response
-        ->getBody()
-        ->write("response");
-
-    return $response;
-
-});
-
 
 $app->start();
