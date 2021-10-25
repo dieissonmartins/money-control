8698c7441dd8432befce7e2a3f7a585468d45d69
Dieisson Martins <dieisson.martins.santos@gmail.com>
2021-10-25 20:26:31 -0300

    add method Http POST router

--- src/Application.php
+++ src/Application.php
@@ -48,6 +48,14 @@ class Application
         return $this;
     }
 
+    public function post($path, $action, $name = null): Application
+    {
+        $routing = $this->service('routing');
+        $routing->post($name,$path,$action);
+
+        return $this;
+    }
+
     public function start(){
 
 
