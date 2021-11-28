9c08751ef60cdaa0ad77d70bd6ef39619fa5e6a5
Dieisson Martins <dieisson.martins.santos@gmail.com>
2021-11-28 19:50:02 -0300

    clear code

--- src/Models/CategoryCost.php
+++ src/Models/CategoryCost.php
@@ -4,9 +4,9 @@ namespace Src\Models;
 
 use Illuminate\Database\Eloquent\Model;
 
-class CategoryCost extends Model 
+class CategoryCost extends Model
 {
-    protected $table    = "category_costs";
+    protected $table = "category_costs";
 
     protected $fillable = [
         'id',
