8523b13ae74c55f5bda679f814b5ab0177483c07
Dieisson Martins <dieisson.martins.santos@gmail.com>
2021-12-04 21:23:51 -0300

    debug

---
+++ src/Models/BillReceive.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Src\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class BillReceive extends Model
+{
+    //Mass Assignment
+    protected $fillable = [
+        'date_launch',
+        'name',
+        'value',
+        'user_id'
+    ];
+}
